e1fbd0cfed1ed1fa8ffe72e59de31c917ed38dec
Pleshz <pleskovd96@gmail.com>
2026-01-21 13:10:41 +0500

    Создал капчу при обновлении пароля. Плешков

--- ajax/recovery.php
+++ ajax/recovery.php
@@ -1,23 +1,38 @@
 <?php
 	session_start();
 	include("../settings/connect_datebase.php");
+	require_once("../libs/autoload.php");
 	
 	$login = $_POST['login'];
 	
-	// ищем пользователя
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."';");
 	
 	$id = -1;
 	if($user_read = $query_user->fetch_row()) {
-		// создаём новый пароль
 		$id = $user_read[0];
 	}
 	
+	if(isset($_POST["g-recaptcha-response"]) == false) {
+		echo "Не прошли капчу";
+		exit;
+	}
+	$Secret = "6Ld3hVEsAAAAAK_afY6r-oTeFFdrDD29KRZELCjW";
+	$Recaptcha = new \ReCaptcha\ReCaptcha($Secret);
+	$Response = $Recaptcha->verify($_POST["g-recaptcha-response"], $_SERVE['REMOTE_ADDR']);
+	if($Response->isSuccess()) {
+		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`)VALUES ('".$login."', '".$password."', 0)");
+	
+		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='"$login."' AND `password`= '".$password."';");
+		$user_new = $query_user->fetch_row();
+		$id = $user_new[0];
+		if($id != -1) $_SESSION['user'] = $id; 
+		echo $id;
+	}
+
 	function PasswordGeneration() {
-		// создаём пароль
-		$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; // матрица
-		$max=10; // количество
-		$size=StrLen($chars)-1; // Определяем количество символов в $chars
+		$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; /
+		$max=10; 
+		$size=StrLen($chars)-1; 
 		$password="";
 		
 		while($max--) {
@@ -28,19 +43,14 @@
 	}
 	
 	if($id != 0) {
-		//обновляем пароль
-		$password = PasswordGeneration();;
-		// проверяем не используется ли пароль 
+		$password = PasswordGeneration();
 		$query_password = $mysqli->query("SELECT * FROM `users` WHERE `password`= '".md5($password)."';");
 		while($password_read = $query_password->fetch_row()) {
-			// создаём новый пароль
 			$password = PasswordGeneration();
 		}
 		// обновляем пароль
 		$mysqli->query("UPDATE `users` SET `password`='".md5($password)."' WHERE `login` = '".$login."'");
-		// отсылаем на почту
-		//mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
 	}
 	
-	echo $id;
+	// echo $id;
 ?>
